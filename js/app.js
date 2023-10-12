160b2c0c18b994d136ed72eb64ef2547224bbc63
mariasolegallina <gallinamariasole@gmail.com>
2023-10-12 17:36:11 +0200

    add - trying to reverse

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
-const userSentence = prompt("Scrivi una parola e scopri se è palindroma")
+// const userSentence = prompt("Scrivi una parola e scopri se è palindroma")
 
 
-function isPalindrome(sentence) {
+// function isPalindrome(sentence) {
+//     const sentenceLowerCase = sentence.toLowerCase
+//     const reversedSentence = sentenceLowerCase.split('').reverse().join('')
 
-    if (sentence === sentence.reverse) {
-        return true
-    } else {
-        return false
-    }
-}
-console.log(isPalindrome(userSentence)) 
+//     if (sentenceLowerCase === reversedSentence) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(isPalindrome(userSentence)) 
+
+const sentence = "Annaciao"
+
+sentenceLowerCase = sentence.toLowerCase()
+console.log(sentenceLowerCase)
+
+const reversedSentence = sentenceLowerCase.split('').reverse().join('')
+console.log(reversedSentence)
