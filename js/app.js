b88480aca09bf100f331a014fd91a4ec212a0c8c
mariasolegallina <gallinamariasole@gmail.com>
2023-10-12 16:57:32 +0200

    add - function first try

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const userSentence = prompt("Scrivi una parola e scopri se è palindroma")
 
+
 function isPalindrome(sentence) {
 
+    if (sentence === sentence.reverse) {
+        return true
+    } else {
+        return false
+    }
 }
-console.log(isPalindrome)
+console.log(isPalindrome(userSentence)) 
